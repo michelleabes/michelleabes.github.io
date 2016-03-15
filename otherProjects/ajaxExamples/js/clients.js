b8146db6a3cb7e81b885deed508a6e2d80ddd46f
michelleabes <michelle_abraham@Michelles-MacBook-Pro-3.local>
2016-03-15 12:19:20 -0400

    lord

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -6,14 +6,16 @@ $(document).ready(function() {
 
     var html= "<table class='table table-hover table-striped'>" + "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-    $.each(data, function (index, item){
+    $.each(data, function(index, item){
 
       html += "<tr>" +
       "<td>" + item.name + "</td>" +
       "<td>" + item.email + "</td>" +
       "<td>" + item.company + "</td>" +
       "</tr>";
-})
+
+    })
+    
       html += "</table>";
       $("#data").append(html);
 

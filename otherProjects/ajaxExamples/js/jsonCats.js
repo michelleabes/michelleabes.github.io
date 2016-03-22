9df3071797016bd474880d7f6287b319aa3bedff
michelleabes <michelle_abraham@Michelles-MacBook-Pro-3.local>
2016-03-22 09:54:17 -0400

    cats

--- otherProjects/ajaxExamples/js/jsonCats.js
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -1,8 +1,38 @@
 $(document).ready(function(){
 
-  $.getJSON("http://michelleabes.github.io/otherProjects/ajaxExamples/jsonDatabase/jsonCats.json", function(data){
+  $.getJSON("jsonDatabase/jsonCats.json", function(data){
 
     console.dir(data);
+    var html ="";
 
+    $.each(data, function(index, item) {
+      html += '<div class="col-md-4">' +
+        '<div class="catName">' + item.type + '</div>' +
+        '<div class="catType">' + item.name + '</div>' +
+        '<div class="catGender">' + item.gender + '</div>' +
+        '<img src= "' + item.image + '"/>';
+
+      html += '</div>';
+
+    }) //each cat
+    $("#catData").append(html);
   })
 })
+
+/*
+//one per cat
+<div class="col-md-4 cat">
+<div class="catName"></div>
+<div class="catType"></div>
+<div class="catGender"></div>
+<img src= ""/>
+<div class="commentsContainer">
+//one per comment
+  <div class="renterName"></div>
+  <div class="renterLocation"></div>
+  <div class="renterStars"></div>
+  //5 stars, some full, some empty
+  </div> //end comments container
+</div> //end cat
+
+*/

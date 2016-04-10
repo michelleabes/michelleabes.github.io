0619948829a59f50c72ef0f13c69965aea7fce80
michelleabes <michelle_abraham@Michelles-MacBook-Pro-3.local>
2016-04-10 01:46:03 -0400

    ...

--- otherProjects/finalProject/js/final.js
+++ otherProjects/finalProject/js/final.js
@@ -14,28 +14,28 @@ $(document).ready(function() {
 
       }) //click
 
-      // Changes the "Name" background to pink when focused on
-      $("#name").on("focus", function() {
-      //  $("#log").append("<br>input focus");
-        $(this).css("background-color", "#FDD7E4");
-      })
+    // Changes the "Name" background to pink when focused on
+    $("#name").on("focus", function() {
+      $("#successMsg").append("<br>input focus");
+      $(this).css("background-color", "#FDD7E4");
+    })
 
-      // Changes the "Name" background back to white
-      .on("blur", function() {
-      //  $("#log").append("<br>input blur");
-        $(this).css("background-color", "#FFF");
-      });
+    // Changes the "Name" background back to white
+    .on("blur", function() {
+      $("#successMsg").append("<br>input blur");
+      $(this).css("background-color", "#FFF");
+    });
 
-      // Mouse Enter - Mouse Leave
-      $("#submitButton").on("mouseenter", function() {
-        // $("#successMsg").append("<br>Button mouseenter");
-        $(this).text("SO COZY!");
-      })
+    // Mouse Enter - Mouse Leave
+    $("#submitButton").on("mouseenter", function() {
+      $("#successMsg").append("<br>Button mouseenter");
+      $(this).text("SO COZY!");
+    })
 
-      .on("mouseleave", function() {
-        // $("#successMsg").append("<br>Button mouseleave");
-        $(this).text("Submit");
-      });
+    .on("mouseleave", function() {
+      $("#successMsg").append("<br>Button mouseleave");
+      $(this).text("Submit");
+    });
 
     //get the parital via JSON, add to page, activiate associating js
     function getPartial(partial) {

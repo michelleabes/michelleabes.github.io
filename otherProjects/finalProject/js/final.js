7911de8fbde773218ebb73cdbb055a4e7d7fdac7
michelleabes <michelle_abraham@Michelles-MacBook-Pro-3.local>
2016-04-10 13:17:09 -0400

    ....

--- otherProjects/finalProject/js/final.js
+++ otherProjects/finalProject/js/final.js
@@ -102,24 +102,24 @@ $(document).ready(function() {
 
     // Changes the "name" background to pink when focused on
     $("#cardNumber").on("focus", function() {
-      //$("#log").append("<br>input focus");
+      $("#log").append("<br>input focus");
       $(this).css("background-color", "#fff4f8");
     })
 
     // Changes the "name" background back to white
     .on("blur", function() {
-      //$("#log").append("<br>input blur");
+      $("#log").append("<br>input blur");
       $(this).css("background-color", "#FFF");
     });
 
     // Mouse Enter - Mouse Leave
     $("#submitButton").on("mouseenter", function() {
-      //$("#log").append("<br>Button mouseenter");
+      $("#log").append("<br>Button mouseenter");
       $(this).text("SO COZY!");
     })
 
     .on("mouseleave", function() {
-      //$("#log").append("<br>Button mouseleave");
+      $("#log").append("<br>Button mouseleave");
       $(this).text("Submit");
     });
 
